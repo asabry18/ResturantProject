fa714ef043e5d567caa00e55bf2ef55355bb0f67
asabry18 <mohamedmahrous581@gmail.com>
2024-09-07 14:59:58 +0300

    Updtates on the coming soon page - Added the contact section - Added the contact headings - Added the contact form and contact fields - Added the "send message" button

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,8 +13,3 @@ const LinksArray = [
 const linksloop = LinksArray.forEach((ele)=>{
     footerLinks.innerHTML+=`<P class="mb-2">${ele}</p>`
 })
-
-// Initialization for ES Users
-import { Input, Ripple, initMDB } from "mdb-ui-kit";
-
-initMDB({ Input, Ripple });
