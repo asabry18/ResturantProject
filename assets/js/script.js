18f6190b84dc18a2a0a49ce7d322c7b0bbf634ae
Ahmed Sabry <mohamedmahrous581@gmail.com>
2024-09-08 23:29:23 +0300

    feat: add team sec and about us

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 let footerLinks = document.querySelector(".footerLinks")
-
 const LinksArray = [
     "About Us",
     "Menu",
@@ -12,4 +11,26 @@ const LinksArray = [
 
 const linksloop = LinksArray.forEach((ele)=>{
     footerLinks.innerHTML+=`<P class="mb-2">${ele}</p>`
+})
+
+let teams = document.querySelector(".teams")
+const teamsinner = [
+    {name: "Cathy Anderson",title: "Chief executive",Image:"assets/images/aboutUsimages/chieff.jpg"},
+    {name: "Mike Dooley",title: "Executive",Image:"assets/images/aboutUsimages/chieff2new.jpg"},
+    {name: "Alextina Jimiey",title: "Food Inspector",Image:"assets/images/aboutUsimages/chieff3new.jpg"}
+]
+
+const teamsecloop = teamsinner.map((ele)=>{
+    teams.innerHTML+=`
+                <div class="col-lg-4 col-md-6 col-12">
+                    <div class="card">
+                        <img src="${ele.Image}" class="card-img-top">
+                        <div class="card-body text-center overflow-hidden">
+                            <h4 class="card-title">${ele.name}</h4>
+                            <p class="text-capitalize opacity-75">${ele.title}</p>
+                            <hr class="opacity-100 w-75">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat facere libero ad blanditiis sunt sint.</p>
+                        </div>
+                    </div>
+                </div>`
 })
