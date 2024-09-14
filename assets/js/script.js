0b630cfd9bf2330713b28c598d84868fbf287e15
Ahmed Sabry <mohamedmahrous581@gmail.com>
2024-09-15 00:13:27 +0300

    fix nav & footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,18 +11,3 @@ onscroll = function(){
         window.scrollTo(0,0)
     }
 }
-let footerLinks = document.querySelector(".footerLinks")
-
-const LinksArray = [
-    "About Us",
-    "Menu",
-    "Blog",
-    "FAQ",
-    "Services",
-    "Gallery",
-    "Contact Us",
-]
-
-const linksloop = LinksArray.forEach((ele)=>{
-    footerLinks.innerHTML+=`<P class="mb-2">${ele}</p>`
-})
