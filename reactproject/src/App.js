b3c79dd834c74bec60f6be39cfc83d1b27afa174
Mazen Rashad <mazenrashadedu@gmail.com>
2024-10-09 23:00:03 +0300

    Revert "Deleted unwanted files or directories"
    This reverts commit 9f37c794cf9de29df6eba3f32432d2ee45bef8a0.
    
    undo

---
+++ reactproject/src/App.js
@@ -0,0 +1,18 @@
+import './index';
+import Header from './components/Header';
+import ContactHeader from './components/ContactHeader';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+    <Header />
+    <ContactHeader />
+    <Contact />
+    <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ reactproject/src/components/ContactHeader.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ContactHeader() {
+  return (
+    <section className="AboutUsBg d-flex flex-column justify-content-center align-items-center">
+      <h1>Contact Us</h1>
+      <p>Home/Contact</p>
+    </section>
+  );
+}
+
+export default ContactHeader;
