5445e1c8713441a7accc57498cb8dba584f40e9d
karim said <karimsaid214@gmail.com>
2024-10-18 22:09:52 +0300

    menu page

---
+++ reactproject/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import SpecialMenu from './components/SpecialMenu';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <SpecialMenu />
+      </header>
+    </div>
+  );
+}
+
+export default App;
