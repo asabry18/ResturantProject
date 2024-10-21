272ed952394b7e2459d9ce335f8aedf957a6cc9c
asabry18 <mohamedmahrous581@gmail.com>
2024-10-21 16:24:02 +0300

    updating to bootstrap react

--- reactproject/src/views/Header.js
+++ reactproject/src/views/Header.js
@@ -2,12 +2,14 @@ import React from "react";
 import logo from "../images/header/logo.png"; // Corrected image path
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeadset } from "@fortawesome/free-solid-svg-icons";
+import { Navbar, Nav, Container } from "react-bootstrap"; // Import React Bootstrap components
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Header() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light p-2">
-      <div className="container">
-        <a className="navbar-brand" href="#">
+    <Navbar bg="light" expand="lg" className="p-2">
+      <Container>
+        <Navbar.Brand href="#">
           <img
             src={logo} // Using imported logo
             alt="Logo"
@@ -15,60 +17,26 @@ function Header() {
             height="30"
             className="d-inline-block align-text-center justify-content-center"
           />
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <a className="nav-link active" href="#">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Menu
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Blog
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                About Us
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Contact Us
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Reservation
-              </a>
-            </li>
-          </ul>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarSupportedContent" />
+        <Navbar.Collapse id="navbarSupportedContent">
+          <Nav className="ms-auto">
+            <Nav.Link href="#" className="active">Home</Nav.Link>
+            <Nav.Link href="#">Menu</Nav.Link>
+            <Nav.Link href="#">Blog</Nav.Link>
+            <Nav.Link href="#">About Us</Nav.Link>
+            <Nav.Link href="#">Contact Us</Nav.Link>
+            <Nav.Link href="#">Reservation</Nav.Link>
+          </Nav>
           <form className="d-flex" role="search">
             <button className="text-black ms-2 border-0" type="submit">
               <FontAwesomeIcon icon={faHeadset} className="fa-xl me-1 custom-icon" />
               +880 123 465 789
             </button>
           </form>
-        </div>
-      </div>
-    </nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
