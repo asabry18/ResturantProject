3d134b9a4e0a8304235a6efe55c42c468cf3b050
asabry18 <mohamedmahrous581@gmail.com>
2024-10-30 14:39:47 +0300

    added node to blog section

--- reactproject/src/components/BlogData.js
+++ reactproject/src/components/BlogData.js
@@ -1,81 +1,29 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 const BlogsSection = () => {
-    const [menu, setMenu] = useState([
-        {
-            backgroundImage: require('../assets/images/latestnews/grands.png')   , 
-            title:'New Restaurant Town Our Ple Award Contract' , 
-            paragraph: 'Drive your business and manage risks with a global industry leader.' , 
-            date:'Aug 1,2023'
-        },
+    const [blogs, setBlogs] = useState([]);
+    const [showMore , setShowMore] = useState(false)
     
-        {
-            backgroundImage: require('../assets/images/latestnews/salad.png') , 
-            title:'Greek Yogurt Breakfast Bowls Ple Award Contract' , 
-            paragraph: 'Start your day right with healthy and nutritious meals' , 
-            date:'Jun 14, 2023'
-        },
-    
-        {
-            backgroundImage: require('../assets/images/latestnews/chicken.png') , 
-            title:'Make Authentic Italian Margherita Pizza at Home' , 
-            paragraph: 'Learn how to craft authentic Italian dishes right at home.' , 
-            date:'Sep 5, 2023'
-        },
-    
-        {
-            backgroundImage: require('../assets/images/latestnews/salad.png') , 
-            title:'Exploring New Food Trends in 2023' , 
-            paragraph: 'Discover the latest food trends shaping the culinary world.' , 
-            date:'Jan 15, 2023'
-        },
-    
-        {
-            backgroundImage: require('../assets/images/latestnews/grands.png') , 
-            title:'Healthy Breakfast Recipes for a Great Start' , 
-            paragraph: 'Explore delicious and nutritious breakfast options.' , 
-            date:'Mar 22, 2023'
-        },
-    
-        {
-            backgroundImage: require('../assets/images/latestnews/chicken.png') , 
-            title:'The Art of Crafting Homemade Pasta' , 
-            paragraph: 'Learn how to make authentic Italian pasta from scratch.' , 
-            date:'May 9, 2023'
-        }
-    ]);
-
-    const showMore = ()=>{
-        let extraMenuItems = [];
-        if (menu.length < 9) {
-            extraMenuItems = [
-                {
-                    backgroundImage: require('../assets/images/latestnews/grands.png')   , 
-                    title:'New Restaurant Town Our Ple Award Contract' , 
-                    paragraph: 'Drive your business and manage risks with a global industry leader.' , 
-                    date:'Aug 1,2023'
-                },
-            
-                {
-                    backgroundImage: require('../assets/images/latestnews/salad.png') , 
-                    title:'Greek Yogurt Breakfast Bowls Ple Award Contract' , 
-                    paragraph: 'Start your day right with healthy and nutritious meals' , 
-                    date:'Jun 14, 2023'
-                },
-            
-                {
-                    backgroundImage: require('../assets/images/latestnews/chicken.png') , 
-                    title:'Make Authentic Italian Margherita Pizza at Home' , 
-                    paragraph: 'Learn how to craft authentic Italian dishes right at home.' , 
-                    date:'Sep 5, 2023'
+    // Fetch Blog by axios
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://localhost:3001/blogs');
+                if (response.status === 200) {
+                    setBlogs(response.data);
+                    console.log(response.data)
                 }
-            ]
-        }
-        
-        setMenu([
-            ...menu,
-            ...extraMenuItems,
-        ])
-    }
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+
+        fetchData(); 
+    }, []);
+
+
+    
+
 
     return (
         <section className="blogSection py-5">
@@ -83,15 +31,15 @@ const BlogsSection = () => {
                 <h5 className='newsTitle'>News & Blogs</h5>
                 <h1>Latest News</h1>
                 <div className="mainBlog">
-                {menu.map(menuItem => (
+                {(showMore ? blogs : blogs.slice(0,6)).map(blog => (
                     <div className="card latestNewsCard">
-                    <img src={menuItem.backgroundImage} className="card-img-top" alt="Restaurant-Latest-News" />
+                    <img src={blog.cardImageUrl} className="card-img-top" alt="Restaurant-Latest-News" />
                     <div className="card-body">
-                        <h5 className="card-title">{menuItem.title}</h5>
-                        <p className="card-text opacity-75">{menuItem.paragraph}</p>
+                        <h5 className="card-title">{blog.title}</h5>
+                        <p className="card-text opacity-75">{blog.body}</p>
                         <hr />
                         <div className="date-blog opacity-75">
-                            <p>{menuItem.date}</p>
+                            <p>{blog.date}</p>
                             <a href="###" className="opacity-75">Read More</a>
                         </div>
                     </div>
@@ -99,7 +47,7 @@ const BlogsSection = () => {
                 )
                 )}
                 </div>
-                {menu.length < 9 && <button onClick={showMore} className="showMore">Show More</button>}
+                {!showMore && <button onClick={()=>{setShowMore(true)}} className="showMore">Show More</button>}
             </div>
         </section>
     );
