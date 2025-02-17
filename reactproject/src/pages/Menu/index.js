03093cdfe52eb4bc3e3f17e184f914a7d33aa50e
Ahmed Sabry <mohamedmahrous581@gmail.com>
2025-02-17 23:06:14 +0200

    fetch menu api

--- reactproject/src/pages/Menu/index.js
+++ reactproject/src/pages/Menu/index.js
@@ -1,70 +1,50 @@
 import MenuItem from "../../Components/MenuItem";
-import dish1 from "../../assets/images/menu/dish1.png";
-import dish2 from "../../assets/images/menu/dish3.png";
-import dish3 from "../../assets/images/menu/dish4.png";
-import dish4 from "../../assets/images/menu/dish5.png";
 import All from "../../assets/images/menu/All.png";
-import vegan from "../../assets/images/menu/Salad.png";
-import breakfast from "../../assets/images/menu/breakfast.png";
-import lunch from "../../assets/images/menu/Lunch.png";
-import seafood from "../../assets/images/menu/seafood.png";
+import VeganIcon from "../../assets/images/menu/Salad.png";
+import BreakfastIcon from "../../assets/images/menu/breakfast.png";
+import LunchIcon from "../../assets/images/menu/Lunch.png";
+import SeafoodIcon from "../../assets/images/menu/seafood.png";
+import axios from "axios";
 import { Row, Col, Container } from "react-bootstrap";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../Menu/Menu.css";
 
-const menuItems = [
-  {
-    id: 1,
-    image: dish1,
-    itemName: "Lumpia with Sauce",
-    category: "Vegan",
-    price: 12,
-    itemDescription:"Lorem ipsum dolor sit, consectetur adipiscing elit, sed do eiusmod tempor",
-  },
-  {
-    id: 2,
-    image: dish2,
-    itemName: "Fish and Veggie",
-    category: "Seafood",
-    price: 15,
-    itemDescription:
-      "Lorem ipsum dolor sit, consectetur adipiscing elit, sed do eiusmod tempor",
-  },
-  {
-    id: 3,
-    image: dish3,
-    itemName: "Tofu Chili",
-    category: "Lunch",
-    price: 10,
-    itemDescription:
-      "Lorem ipsum dolor sit, consectetur adipiscing elit, sed do eiusmod tempor",
-  },
-  {
-    id: 4,
-    image: dish4,
-    itemName: "Egg and Cocumber",
-    category: "Breakfast",
-    price: 10,
-    itemDescription:
-      "Lorem ipsum dolor sit, consectetur adipiscing elit, sed do eiusmod tempor",
-  },
-];
-
-const categories = [
-  { name: "All", icon: All },
-  { name: "Vegan", icon: vegan },
-  { name: "Seafood", icon: seafood },
-  { name: "Lunch", icon: lunch },
-  { name: "Breakfast", icon: breakfast },
-];
+const categoryIcons = {
+  All: All,
+  Vegan: VeganIcon,
+  Breakfast: BreakfastIcon,
+  Lunch: LunchIcon,
+  Seafood: SeafoodIcon,
+};
 
 export default function Menu() {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [menu, setMenu] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get('http://localhost:3001/menu');
+      if (response.status === 200) {
+        setMenu(response.data);
+        const uniqueCategories = [...new Set(response.data.map(item => item.category))];
+
+        setCategories([
+          { name: "All", icon: categoryIcons["All"] },
+            ...uniqueCategories.map(category => ({
+            name: category,
+            icon: categoryIcons[category]
+          })),
+        ]);
+      }
+    };
+    fetchData();
+  }, []);
 
   const filteredItems =
     selectedCategory === "All"
-      ? menuItems
-      : menuItems.filter((item) => item.category === selectedCategory);
+      ? menu
+      : menu.filter((item) => item.category === selectedCategory);
 
   return (
     <>
@@ -80,17 +60,9 @@ export default function Menu() {
 
           <Container className="d-flex flex-wrap justify-content-center gap-3 pb-4">
             {categories.map((category) => (
-              <div
-                key={category.name}
-                className={`category ${selectedCategory === category.name ? "active" : ""} d-flex flex-column align-items-center px-3 py-2`}
-                onClick={() => setSelectedCategory(category.name)}
-              >
+              <div key={category.name} className={`category ${selectedCategory === category.name ? "active" : ""} d-flex flex-column align-items-center px-3 py-2`} onClick={() => setSelectedCategory(category.name)}>
                 {category.icon && (
-                  <img
-                    src={category.icon}
-                    alt={`${category.name} icon`}
-                    className="category-icon"
-                  />
+                  <img src={category.icon} alt={`${category.name} icon`} className="category-icon"/>
                 )}
                 {category.name}
               </div>
@@ -99,19 +71,8 @@ export default function Menu() {
 
           <Row className="menuItems py-5 my-3">
             {filteredItems.map((item) => (
-              <Col
-                sm="6"
-                md="4"
-                lg="3"
-                className="menuCol m-auto"
-                key={item.id}
-              >
-                <MenuItem
-                  itemName={item.itemName}
-                  itemDescription={item.itemDescription}
-                  price={item.price}
-                  image={item.image}
-                />
+              <Col sm="6" md="4" lg="3" className="menuCol m-auto" key={item.id}>
+                <MenuItem itemName={item.name} price={item.price} itemDescription={item.description} image={item.imageUrl}/>
               </Col>
             ))}
           </Row>
