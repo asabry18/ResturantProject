6996f283d3a62ee802deb4673c733aa842969446
asabry18 <mohamedmahrous581@gmail.com>
2025-05-19 14:49:19 +0300

    t3bt wlahy

--- reactproject/src/App.js
+++ reactproject/src/App.js
@@ -35,7 +35,7 @@ function App() {
 
       try {
         const validationRequest = await axios.post(
-          "http://localhost:3001/api/verifyToken",
+          "https://cafeu-api.vercel.app/api/verifyToken",
           { token: authToken },
         );
         setIsUserAdmin(validationRequest.data.isAdmin ?? false);
